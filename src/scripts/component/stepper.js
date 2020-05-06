a1bb953ba21fb327abbb410083583c3fec4fafa8
abdelali-code <57986145+abdelali-code@users.noreply.github.com>
2020-05-06 09:35:55 +0000

    nothing here

--- src/scripts/component/stepper.js
+++ src/scripts/component/stepper.js
@@ -1,51 +1,50 @@
 import { createElement } from "../helpers/createElement";
-import { updateStepper } from "./updateStepper";
 
 function addStepper() {
   const container = createElement("div", {
     class: ["container"],
     id: "stepper",
   });
-  // const container2 = createElement("div", {
-  //   class: ["progress-bar"],
-  //   id: "progress-bar"
-  // });
-  // const sp1 = createElement("span", {
-  //   class: ["big-circle", "add_main_color"],
-  //   id: "sp1"
-  // });
-  // const i1 = createElement("i", {
-  //   class: ["circle", "add_warning_color"],
-  //   id: "c1"
-  // });
-  // sp1.appendChild(i1);
+  const container2 = createElement("div", {
+    class: ["progress-bar"],
+    id: "progress-bar"
+  });
+  const sp1 = createElement("span", {
+    class: ["big-circle", "add_main_color"],
+    id: "sp1"
+  });
+  const i1 = createElement("i", {
+    class: ["circle", "add_warning_color"],
+    id: "c1"
+  });
+  sp1.appendChild(i1);
 
-  // // second step
-  // const line = createElement("div", {
-  //   class: ["line"]
-  // })
-  // const sp2 = createElement("span", {
-  //   class: ["big-circle", "second-circle-container"],
-  //   id: "sp2"
-  // });
-  // const i2 = createElement("i", {
-  //   class: ["circle", "second-circle"],
-  //   id: "c2"
-  // });
-  // sp2.appendChild(i2);
-  // line.appendChild(sp2);
-  // // third circle
-  // const sp3 = createElement("span", {
-  //   class: ["big-circle"],
-  //   id: "sp3"
-  // });
-  // const i3 = createElement("i", {
-  //   class: ["circle"],
-  //   id: "c3"
-  // });
-  // sp3.appendChild(i3);
-  // addToparent(container2, [sp1, line, sp3]);
-  // container.appendChild(container2);
+  // second step
+  const line = createElement("div", {
+    class: ["line"]
+  })
+  const sp2 = createElement("span", {
+    class: ["big-circle", "second-circle-container"],
+    id: "sp2"
+  });
+  const i2 = createElement("i", {
+    class: ["circle", "second-circle"],
+    id: "c2"
+  });
+  sp2.appendChild(i2);
+  line.appendChild(sp2);
+  // third circle
+  const sp3 = createElement("span", {
+    class: ["big-circle"],
+    id: "sp3"
+  });
+  const i3 = createElement("i", {
+    class: ["circle"],
+    id: "c3"
+  });
+  sp3.appendChild(i3);
+  addToparent(container2, [sp1, line, sp3]);
+  container.appendChild(container2);
   container.innerHTML = `
             <div class="progress-bar" id="progress-bar">
               <span class="big-circle add_main_color" id="sp1"><i class="circle add_warning_color" id="c1"></i></span>
@@ -61,7 +60,7 @@ function addStepper() {
 
   return container;
 }
-// 
+//
 // 
 // 
 // 
@@ -70,37 +69,37 @@ function addStepper() {
 // 
 // 
 
-function stepper(node1, node2, node3, root) {
-  let counter = 1;
+// function stepper(node1, node2, node3, root) {
+//   let counter = 1;
 
-  // add stepper indicateur
-  const step = addStepper();
-  // update content of the root
-  const next = document.createElement("button");
-  next.textContent = "next";
+//   // add stepper indicateur
+//   const step = addStepper();
+//   // update content of the root
+//   const next = document.createElement("button");
+//   next.textContent = "next";
 
-  root.appendChild(step);
-  root.appendChild(node1);
-  root.appendChild(next);
+//   root.appendChild(step);
+//   root.appendChild(node1);
+//   root.appendChild(next);
 
-  next.addEventListener("click", () => {
-    counter++;
-    switch (counter) {
-      case 2:
-        root.replaceChild(node2, node1);
-        updateStepper(1, 2);
-        break;
-      case 3:
-        root.replaceChild(node3, node2);
-        updateStepper(2, 3);
-        counter = 0;
-        break;
-      case 1:
-        root.replaceChild(node1, node3);
-        updateStepper(3, 1);
-        break;
-    }
-  })
-}
+//   next.addEventListener("click", () => {
+//     counter++;
+//     switch (counter) {
+//       case 2:
+//         root.replaceChild(node2, node1);
+//         updateStepper(1, 2);
+//         break;
+//       case 3:
+//         root.replaceChild(node3, node2);
+//         updateStepper(2, 3);
+//         counter = 0;
+//         break;
+//       case 1:
+//         root.replaceChild(node1, node3);
+//         updateStepper(3, 1);
+//         break;
+//     }
+//   })
+// }
 
-export { stepper };
+// export { stepper };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,26 @@
 "use strict";
-import { stepper } from "./component/stepper";
-import { addPreambule } from "./component/peambule";
+import { addProgressIndi } from "./component/addProgress";
+import { addStepper } from "./component/stepper";
+import { addToparent, createElement } from "./helpers/createElement";
+import { startBtn, addPreambule } from "./component/peambule";
+import { addQuiz, addSiQs } from "./component/addQuiz";
+import { addRecommandations } from "./component/addRecommand";
+import { QUESTION } from "./helpers/question";
+import { updateStepper } from "./component/updateStepper";
+import { collectAnswer } from "./component/collectAnswer";
+import { updateProgress } from "./component/updateProgress";
+import { checkAnswer, isLess15 } from "./component/checkAnswer";
+import { addMsgs, displaMsg } from "./component/displayMsg";
 
+//
+//
+//
+//
+//
+//
+//
+//
+//
 //
 //
 //
@@ -16,287 +35,246 @@ import { addPreambule } from "./component/peambule";
 
 window.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  let counter = 0;
+  // to store the answer of the question;
+  // Array(QUESTION.length).fill(null);
+  let result = Array(23).fill(null);
+  // create a stepper
+  const stepper = addStepper();
+  // create the start button
+  const start = startBtn();
+  // create preambule
   const preambule = addPreambule();
+  // create the progress indicateur
+  const addProgressIndice = addProgressIndi();
+  // create the recommandations section
+  const addRecm = addRecommandations();
+
+  // the finish btn
+  const submitBtn = createElement("button", {
+    class: [
+      "btn",
+      "small-btn",
+      "btn-main",
+      "question--btn",
+      "add-green-backgr",
+    ],
+  });
+  //
+  //
+  //
+  submitBtn.textContent = "Submit";
+
+  // the restart btn
+  const restartBtn = createElement("button", {
+    class: ["btn", "small-btn", "btn-main", "reprendre"],
+  });
+  restartBtn.textContent = "Repredre Test";
+
+  // remove of all content from the root
   root.innerHTML = "";
-  const div2 = document.createElement("div");
-  div2.textContent = "hello world 2";
-  const div3 = document.createElement("div");
-  div3.textContent = "hello world 3";
-  stepper(preambule, div2, div3, root);
-})
-//
-//
-//
-//
-//
-//
-//
-//
-//
+  // add the content of inferface of the test;
+  addToparent(root, [stepper, preambule, start, addRecm]);
 
-// window.addEventListener("DOMContentLoaded", () => {
-//   const root = document.getElementById("root");
-//   let counter = 0;
-//   let QUESTION;
-//   // to store the answer of the question;
-//   // Array(QUESTION.length).fill(null);
-//   let result = Array(23).fill(null);
-//   // create a stepper
-//   const stepper = addStepper();
-//   // create the start button
-//   const start = startBtn();
-//   // create preambule
-//   const preambule = addPreambule();
-//   // create the progress indicateur
-//   const addProgressIndice = addProgressIndi();
-//   // create the recommandations section
-//   const addRecm = addRecommandations();
+  // function responsable of adding the question to user
+  let addQs = addQuiz();
 
-//   // the finish btn
-//   const submitBtn = createElement("button", {
-//     class: [
-//       "btn",
-//       "small-btn",
-//       "btn-main",
-//       "question--btn",
-//       "add-green-backgr",
-//     ],
-//   });
-//   //
-//   //
-//   //
-//   submitBtn.textContent = "Submit";
+  /*********************** when user start the quiz **********************************/
 
-//   // the restart btn
-//   const restartBtn = createElement("button", {
-//     class: ["btn", "small-btn", "btn-main", "reprendre"],
-//   });
-//   restartBtn.textContent = "Repredre Test";
+  start.addEventListener("click", () => {
+    // add the first question
+    addSiQs(
+      counter,
+      QUESTION,
+      addQs.para,
+      addQs.form,
+      addQs.warning_cont,
+      result
+    );
+    // move stepper from informations to Questionnaire
+    updateStepper(1, 2);
+    // remove the preambule from test interface
+    root.removeChild(preambule);
+    // remove the start button
+    root.removeChild(start);
+    // add progress indicateur and question container and recommadations section to the test page
+    addToparent(root, [stepper, addProgressIndice, addQs.parent, addRecm]);
+    // check progress indicateur as one question is answered
+    updateProgress(counter);
+  });
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  /*********************** end of when user start the quiz **********************************/
 
-//   // remove of all content from the root
-//   root.innerHTML = "";
-//   // add the content of inferface of the test;
-//   addToparent(root, [stepper, preambule, start, addRecm]);
+  // *************************move to the next question ******************************/
+  //   when we click on btn suiv incre counter by 1 to change to the next question
+  addQs.nextBtn.addEventListener("click", () => {
+    if (counter <= QUESTION.length - 1) {
+      // collect the first user choice
+      collectAnswer(result, counter);
+      // for age is less than 15
+      if (counter == 0) {
+        let is_accepted = isLess15(result, 0);
+        if (is_accepted) {
+          result = Array(23).fill(null);
+          updateStepper(2, 3);
+          root.removeChild(addProgressIndice);
+          // display the result of the test to the user
+          root.replaceChild(displaMsg(is_accepted), addQs.parent);
+          root.insertBefore(restartBtn, addRecm);
+          return;
+        }
+      }
+      // end age
+      if (
+        (counter === 1 && result[1] == "Non") ||
+        (counter === 7 && (result[7] === "Non" || result[7] == null))
+      ) {
+        counter++;
+      }
+    }
 
-//   // function responsable of adding the question to user
-//   let addQs = addQuiz();
+    // while counter not reach the end of the question do this
+    if (counter < QUESTION.length - 1) {
+      // move counter by one to indicate to the next question
+      counter++;
+      // when counter is increment dispay the appropriate question
+      addSiQs(
+        counter,
+        QUESTION,
+        addQs.para,
+        addQs.form,
+        addQs.warning_cont,
+        result
+      );
+      // and again update progress bar
+      updateProgress(counter);
+    }
+    // insert submit btn when user reach the last question
+    if (counter === QUESTION.length - 1) {
+      // replace the next question btn by submit button
+      addQs.btnCont.replaceChild(submitBtn, addQs.nextBtn);
+    }
+  });
 
-//   /*********************** when user start the quiz **********************************/
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  /*************************end of move to the next question ******************************/
 
-//   start.addEventListener("click", () => {
-//     if (localStorage.getItem("langue") === "arabe") {
-//       QUESTION = QUESTION_AR;
-//     }
-//     else {
-//       QUESTION = question;
-//     }
-//     // add the first question
-//     addSiQs(
-//       counter,
-//       QUESTION,
-//       addQs.para,
-//       addQs.form,
-//       addQs.warning_cont,
-//       result
-//     );
-//     // move stepper from informations to Questionnaire
-//     updateStepper(1, 2);
-//     // remove the preambule from test interface
-//     root.removeChild(preambule);
-//     // remove the start button
-//     root.removeChild(start);
-//     // add progress indicateur and question container and recommadations section to the test page
-//     addToparent(root, [stepper, addProgressIndice, addQs.parent, addRecm]);
-//     // check progress indicateur as one question is answered
-//     updateProgress(counter);
-//   });
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   /*********************** end of when user start the quiz **********************************/
+  /*************************return the previous question ******************************/
+  //   when we click on prev question btn decrement counter by 1 to change to prev question
+  addQs.prevBtn.addEventListener("click", () => {
+    if (counter > 0) {
+      if (
+        (counter === 3 && result[1] === "Non") ||
+        (counter === 9 && (result[7] === "Non" || result[7] == null))
+      ) {
+        counter--;
+      }
+      counter--;
+      // when counter is decrement dispay the appropriate question
+      addSiQs(
+        counter,
+        QUESTION,
+        addQs.para,
+        addQs.form,
+        addQs.warning_cont,
+        result
+      );
+      updateProgress(counter);
+    }
+    // remove submit btn
+    if (counter === QUESTION.length - 2) {
+      addQs.btnCont.replaceChild(addQs.nextBtn, submitBtn);
+    }
+  });
+  /*************************end of return to the previous question ******************************/
 
-//   // *************************move to the next question ******************************/
-//   //   when we click on btn suiv incre counter by 1 to change to the next question
-//   addQs.nextBtn.addEventListener("click", () => {
-//     if (localStorage.getItem("langue") === "arabe") {
-//       QUESTION = QUESTION_AR;
-//     }
-//     else {
-//       QUESTION = question;
-//     }
-//     if (counter <= QUESTION.length - 1) {
-//       // collect the first user choice
-//       collectAnswer(result, counter);
-//       // for age is less than 15
-//       if (counter == 0) {
-//         let is_accepted = isLess15(result, 0);
-//         if (is_accepted) {
-//           result = Array(23).fill(null);
-//           updateStepper(2, 3);
-//           root.removeChild(addProgressIndice);
-//           // display the result of the test to the user
-//           root.replaceChild(displaMsg(is_accepted), addQs.parent);
-//           root.insertBefore(restartBtn, addRecm);
-//           return;
-//         }
-//       }
-//       // end age
-//       if (
-//         (counter === 1 && result[1] == "Non") ||
-//         (counter === 7 && (result[7] === "Non" || result[7] == null))
-//       ) {
-//         counter++;
-//       }
-//     }
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  /***************************when user click the submit btn;***********************/
+  submitBtn.addEventListener("click", () => {
+    let answer = checkAnswer(result);
+    updateStepper(2, 3);
+    root.removeChild(addProgressIndice);
+    // display the result of the test to the user
+    root.replaceChild(addMsgs(answer), addQs.parent);
+    // add a button to restart the test
+    root.insertBefore(restartBtn, addRecm);
+  });
+  /*************************** end when user click the submit btn;*********************/
 
-//     // while counter not reach the end of the question do this
-//     if (counter < QUESTION.length - 1) {
-//       // move counter by one to indicate to the next question
-//       counter++;
-//       // when counter is increment dispay the appropriate question
-//       addSiQs(
-//         counter,
-//         QUESTION,
-//         addQs.para,
-//         addQs.form,
-//         addQs.warning_cont,
-//         result
-//       );
-//       // and again update progress bar
-//       updateProgress(counter);
-//     }
-//     // insert submit btn when user reach the last question
-//     if (counter === QUESTION.length - 1) {
-//       // replace the next question btn by submit button
-//       addQs.btnCont.replaceChild(submitBtn, addQs.nextBtn);
-//     }
-//   });
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  /**************** repredre the test ********************************************/
+  restartBtn.addEventListener("click", () => {
+    // reset counter and result to the initial value to allow user to retake test
+    counter = 0;
+    result = Array(23).fill(null);
+    // remove all the content from the root
+    root.innerHTML = "";
+    // add the contetn of the acceuil page of the test
+    addToparent(root, [stepper, preambule, start, addRecm]);
+    // update the design of the stepper
+    updateStepper(3, 1);
 
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   /*************************end of move to the next question ******************************/
-
-//   /*************************return the previous question ******************************/
-//   //   when we click on prev question btn decrement counter by 1 to change to prev question
-//   addQs.prevBtn.addEventListener("click", () => {
-//     if (localStorage.getItem("langue") === "arabe") {
-//       QUESTION = QUESTION_AR;
-//     }
-//     else {
-//       QUESTION = question;
-//     }
-//     if (counter > 0) {
-//       if (
-//         (counter === 3 && result[1] === "Non") ||
-//         (counter === 9 && (result[7] === "Non" || result[7] == null))
-//       ) {
-//         counter--;
-//       }
-//       counter--;
-//       // when counter is decrement dispay the appropriate question
-//       addSiQs(
-//         counter,
-//         QUESTION,
-//         addQs.para,
-//         addQs.form,
-//         addQs.warning_cont,
-//         result
-//       );
-//       updateProgress(counter);
-//     }
-//     // remove submit btn
-//     if (counter === QUESTION.length - 2) {
-//       addQs.btnCont.replaceChild(addQs.nextBtn, submitBtn);
-//     }
-//   });
-//   /*************************end of return to the previous question ******************************/
-
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   /***************************when user click the submit btn;***********************/
-//   submitBtn.addEventListener("click", () => {
-//     let answer = checkAnswer(result);
-//     updateStepper(2, 3);
-//     root.removeChild(addProgressIndice);
-//     // display the result of the test to the user
-//     root.replaceChild(addMsgs(answer), addQs.parent);
-//     // add a button to restart the test
-//     root.insertBefore(restartBtn, addRecm);
-//   });
-//   /*************************** end when user click the submit btn;*********************/
-
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   //
-//   /**************** repredre the test ********************************************/
-//   restartBtn.addEventListener("click", () => {
-//     // reset counter and result to the initial value to allow user to retake test
-//     counter = 0;
-//     result = Array(23).fill(null);
-//     // remove all the content from the root
-//     root.innerHTML = "";
-//     // add the contetn of the acceuil page of the test
-//     addToparent(root, [stepper, preambule, start, addRecm]);
-//     // update the design of the stepper
-//     updateStepper(3, 1);
-
-//     // replace the submit button by the next question
-//     addQs.btnCont.replaceChild(addQs.nextBtn, submitBtn);
-//   });
-//   /****************end repredre the test********************************************/
-// });
-
-
+    // replace the submit button by the next question
+    addQs.btnCont.replaceChild(addQs.nextBtn, submitBtn);
+  });
+  /****************end repredre the test********************************************/
+});
